2ea8de1f283b4eeda9d61c0297da9ce834ca3386
Steve Marquez <steveantonio00@gmail.com>
2020-05-12 02:16:38 -0700

    Successfully render one module with data via proxy server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3002;
-
+const port = 3005;
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '../public/')));
+
+app.get('/api/images/:id', (req, res) => {
+  axios.get(`http://localhost:3001/api/images/${req.params.id}`)
+      .then((response) => {
+        res.send(response.data)
+      }).catch((err) => {
+        console.log('image')
+          res.send(err)
+      })
+})
 
 app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`))
