06fdeffc78f4df02d00ffee4fe45a055bf6a7164
Steve Marquez <steveantonio00@gmail.com>
2020-05-12 14:18:34 -0700

    Render reviews component in proxy server

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const port = 3005;
 const axios = require('axios');
+const cors = require('cors');
 
 app.use(express.static(path.join(__dirname, '../public/')));
-
+app.use(cors());
 // Gallery Component
+
 app.get('/api/images/:id', (req, res) => {
   axios.get(`http://localhost:3001/api/images/${req.params.id}`)
       .then((response) => {
@@ -16,10 +18,6 @@ app.get('/api/images/:id', (req, res) => {
           res.send(err)
       })
 })
-
-
-
-
 
 // Product Component
 
@@ -33,7 +31,7 @@ app.get('/product/:id', (req, res) => {
   })
 });
 
-
+// review
 app.get('/product/:id/find-store', (req, res) => {
   axios.get(`http://localhost:3000/product/${req.params.id}/${req.query.q}`)
   .then((response) => {
@@ -44,5 +42,27 @@ app.get('/product/:id/find-store', (req, res) => {
   })
 });
 
+
+
+// reviews
+app.get('/api/products/:product_id/reviews', (req, res) => {
+  axios.get(`http://localhost:8080/api/products/${req.params.productId}/reviews`)
+  .then((response) => {
+    res.send(response.data)
+  }).catch((err) => {
+    console.log('find-stores')
+      res.send(err);
+  })
+})
+
+app.get('/api/products/:product_id/reviews/:review_id', (req, res) => {
+  axios.get(`http://localhost:8080/api/products/${req.params.productId}/reviews/${req.params.reviewId}`)
+  .then((response) => {
+    res.send(response.data)
+  }).catch((err) => {
+    console.log('find-stores')
+      res.send(err);
+  })
+})
 
 app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`))
