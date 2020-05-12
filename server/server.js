8ccd0509a08d08966d06aa5a0ead27513b08cd21
Steve Marquez <steveantonio00@gmail.com>
2020-05-12 12:12:45 -0700

    Implement basic css and render product component

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '../public/')));
 
+// Gallery Component
 app.get('/api/images/:id', (req, res) => {
   axios.get(`http://localhost:3001/api/images/${req.params.id}`)
       .then((response) => {
@@ -15,5 +16,33 @@ app.get('/api/images/:id', (req, res) => {
           res.send(err)
       })
 })
+
+
+
+
+
+// Product Component
+
+app.get('/product/:id', (req, res) => {
+  axios.get(`http://localhost:3000/product/${req.params.id}`)
+  .then((response) => {
+    res.send(response.data)
+  }).catch((err) => {
+    console.log('product')
+      res.send(err)
+  })
+});
+
+
+app.get('/product/:id/find-store', (req, res) => {
+  axios.get(`http://localhost:3000/product/${req.params.id}/${req.query.q}`)
+  .then((response) => {
+    res.send(response.data)
+  }).catch((err) => {
+    console.log('find-stores')
+      res.send(err);
+  })
+});
+
 
 app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`))
