2ac671895440bcca0291b9fc486e6fbfae610ac3
Steve Marquez <steveantonio00@gmail.com>
2020-05-11 20:18:12 -0700

    Serve static html

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3002
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3002;
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`))
