e3fa7ef62243e54a68d712516e070e16f453468a
Steve Marquez <steveantonio00@gmail.com>
2020-05-13 16:39:49 -0700

    Functional proxy server

--- server/server.js
+++ server/server.js
@@ -1,69 +1,91 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
+
 const port = 3005;
 const axios = require('axios');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public/')));
-// Gallery Component
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/images/:id', (req, res) => {
-  axios.get(`http://localhost:3001/api/images/${req.params.id}`)
-  .then((response) => {
-    res.send(response.data)
-  }).catch((err) => {
-    console.log('image')
-          res.send(err)
-        })
-})
-
-// Product Component
+  axios
+    .get(`http://localhost:3001/api/images/${req.params.id}`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.get('/product/:id', (req, res) => {
-  axios.get(`http://localhost:3000/product/${req.params.id}`)
-  .then((response) => {
-    res.send(response.data)
-  }).catch((err) => {
-    console.log('product')
-      res.send(err)
+  axios
+    .get(`http://localhost:3002/product/${req.params.id}`)
+    .then((response) => {
+      res.status(200).send(response.data);
     })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
-// product
 app.get('/product/:id/:find-store', (req, res) => {
-  axios.get(`http://localhost:3000/product/${req.params.id}/${req.query.find-store}`)
-  .then((response) => {
-    res.send(response.data)
-  }).catch((err) => {
-    console.log('find-stores')
-      res.send(err);
-  })
+  axios
+    .get(
+      `http://localhost:3002git /product/${req.params.id}/find-store/?q=${req.query.q}`,
+    )
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 });
 
-
-// // reviews
 app.get('/api/products/:product_id/reviews', (req, res) => {
-  axios.get(`http://localhost:8080/api/products/${req.params.product_id}/reviews`)
-  .then((response) => {
-    res.send(response.data)
-  }).catch((err) => {
-    console.log('find-stores')
-    res.send(err);
-  })
-})
+  axios
+    .get(`http://localhost:8080/api/products/${req.params.product_id}/reviews`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.get('/api/products/:product_id/reviews/:review_id', (req, res) => {
-  axios.get(`http://localhost:8080/api/products/${req.params.product_id}/reviews/${req.params.review_id}`)
-  .then((response) => {
-    res.send(response.data)
-  }).catch((err) => {
-    console.log('find-stores')
-    res.send(err);
+  axios
+    .get(
+      `http://localhost:8080/api/products/${req.params.product_id}/reviews/${req.params.review_id}`,
+    )
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.put('/api/products/:product_id/reviews/:review_id', (req, res) => {
+  axios({
+    method: 'PUT',
+    url: `http://localhost:8080/api/products/${req.params.product_id}/reviews/${req.params.review_id}`,
+    data: req.body,
   })
-})
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
-
-app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`))
+app.listen(port, () => console.log(`Proxy server: ----- http://localhost:${port}`));
